cbae138c59f0ad0e6f4e6b8c7713fc00cff8b977
Joel Messerli <hi.github@peg.nu>
2019-07-14 14:54:05 +0200

    Add errorhandling and logging to filefetcher

--- filefetcher/filefetcher.go
+++ filefetcher/filefetcher.go
@@ -3,16 +3,35 @@ package filefetcher
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
-func FetchFile(filepath, nxtoken, baseurl string) {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", baseurl+filepath, nil)
-	req.Header.Set("x-nx-token", nxtoken)
-	res, _ := client.Do(req)
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(body))
+var logger = log.New(os.Stdout, "[file_fetcher] ", log.LstdFlags)
 
+func FetchFile(filePath, nxToken, baseURL string) ([]byte, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s", baseURL, filePath), nil)
+	if err != nil {
+		logger.Printf("Could not create request: %v", err)
+		return nil, err
+	}
+	req.Header.Set("x-nx-token", nxToken)
+
+	res, err := client.Do(req)
+	if err != nil {
+		logger.Printf("Could not execute request: %v", err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		logger.Printf("Could not read request body: %v", err)
+		return nil, err
+	}
+
+	return body, nil
 }
